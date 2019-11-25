852a41be6aab544aee0a03ce59f98018d5de9e5e
colonelwatch <53552802+colonelwatch@users.noreply.github.com>
2019-11-24 16:30:56 -0800

    Create esp32_hr2.ino

---
+++ esp32_hr2/esp32_hr2.ino
@@ -0,0 +1,99 @@
+#include "BluetoothSerial.h"
+
+#define MAX_SAFE_HR 100
+#define MIN_SAFE_HR 40
+
+#define THRESHOLD 75
+#define POLLING_RATE 100 // Hz
+#define DEBOUNCE_INTERVAL 300 // ms
+#define BLINK_INTERVAL 300 // ms
+#define RED_LED 16
+#define BLUE_LED 17
+#define GREEN_LED 18
+#define ONBOARD_LED 2
+
+int sample_period = 1000000/POLLING_RATE;
+
+int tempsum, average;
+bool pulse_tracking = false;
+int last_beat = 0;
+volatile int record[30] = {0};
+volatile int record_index = 0;
+
+volatile int led_number = GREEN_LED;
+
+int values[100];
+int values_index = 0;
+
+unsigned long blink_time = 0;
+
+// Core 0 Interrupt thread
+hw_timer_t * timer = NULL;
+void IRAM_ATTR onTimer(){
+  int reading = analogRead(35);
+  values[values_index] = reading;
+  values_index++;
+  // If the index has reached 99, this is only possible if a second has passed
+  if(values_index == 99){
+    record_index++;
+    record_index%=30;
+    record[record_index] = 0;
+  }
+  values_index %= 100;
+  tempsum = 0;
+  for(int i = 0; i < 100; i++) tempsum += values[i];
+  average = tempsum / 100;
+  if(reading > average + THRESHOLD && millis() > last_beat + DEBOUNCE_INTERVAL){
+    pulse_tracking = true;
+    digitalWrite(led_number, HIGH);
+    blink_time = millis();
+  }
+  else if(pulse_tracking){
+    pulse_tracking = false;
+    last_beat = millis();
+    record[record_index]++;
+    digitalWrite(led_number, LOW);
+  }
+  // So far, motion distorts the signal, how should we account for this?
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+
+  // Just follow pulse until BOOT button is pressed, this lets user get device on wrist
+  //  before the ESP reads garbage data
+  while(digitalRead(0)){}
+  
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, sample_period, true);
+  timerAlarmEnable(timer);
+
+  pinMode(2, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(GREEN_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  
+  for(int i = 0; i < 100; i++){
+    values[i] = analogRead(35);
+    delay(1);
+  }
+}
+
+bool thirty_passed = false;
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  delay(1000);
+  if(record_index == 29) thirty_passed = true;
+  int sum = 0;
+  for(int i = 0; i < 30; i++) sum += record[i];
+  int hr = sum*2;
+  if(thirty_passed == true){
+    if(hr > MAX_SAFE_HR || hr < MIN_SAFE_HR) led_number = RED_LED;
+    else led_number = GREEN_LED;
+    
+    Serial.println(hr);
+  }
+}
